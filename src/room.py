0344d202a799c284d4f9ab9d2120215800295f6c
guillem.barroso <guillem.barroso@ansys.com>
2023-01-20 04:48:19 +0100

    Use three characters to define 30x30 blocks in room maps

--- src/room.py
+++ src/room.py
@@ -10,26 +10,27 @@ class Room:
 
     def __init__(self, screen, ROOM_MAP=None, ROOM_NAME=None):
         """Initialize map class."""
-        self.cells_size = (10, 10)
+        self.cells_size = (30, 30)
         self.walls = []
         self.doors = []
         self.map = ROOM_MAP
         self.name = ROOM_NAME
         self.n_cells_x = screen.surface.get_width() // self.cells_size[0]
         self.n_cells_y = screen.surface.get_height() // self.cells_size[1]
-        self._check_map_size()
+        # self._check_map_size()
         self._build()
 
     def _build(self):
         """Build room map creating walls."""
         x, y = 0, 0
         for raw in self.map:
-            for cell in raw:
-                if cell == "X":
+            for i in range(0, len(raw), 3):
+                cell = raw[i : i + 3]
+                if cell == "XXX":
                     self.walls.append(
                         pygame.Rect(x, y, self.cells_size[0], self.cells_size[1])
                     )
-                if cell == "D":
+                if cell == "D00":
                     self.doors.append(
                         pygame.Rect(x, y, self.cells_size[0], self.cells_size[1])
                     )
@@ -46,7 +47,7 @@ class Room:
             raise ValueError(text)
 
         for i_raw, raw in enumerate(self.map):
-            if not len(raw) == self.n_cells_x:
+            if not len(raw) // 3 == self.n_cells_x:
                 text = (
                     f"Number of columns ({len(raw)}) in raw {i_raw} does not "
                     + f"match the number of the x cells of the screen ({self.n_cells_x})"
